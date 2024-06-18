1aeef45ff6fdb2d8483fab7ee0443081e31108f0
kikusachi <rstoyo3.kikuchi.sachie@techcamp-training.com>
2024-06-18 14:09:41 +0900

    確認問題用ファイル(phpファイル)の追加

---
+++ html/question.php
@@ -0,0 +1,19 @@
+<?php
+    // PHPコードを記述します
+    //確認する際はxamppのApache
+    $name = "Alice";
+    echo "<p>Hello, $name!</p>";
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1>マナーなどの確認問題</h1>
+
+
+        <p>こんにちは！</p>
+    </body>
+</html>
